d397c1e1c1e615265b3de38c98bfb55dacea4382
agospodinov <a.gospodinov@ibs.bg>
2021-02-19 15:10:53 +0200

    maven cleanup

--- nexu-bundle/src/main/resources/NexU-Startup.bat
+++ nexu-bundle/src/main/resources/NexU-Startup.bat
@@ -1,3 +1,4 @@
+@echo off
 rem Â© Nowina Solutions, 2015-2015
 rem
 rem ConceÌ�deÌ�e sous licence EUPL, version 1.1 ou â€“ deÌ€s leur approbation par la Commission europeÌ�enne - versions ulteÌ�rieures de lâ€™EUPL (la Â«LicenceÂ»).
@@ -10,5 +11,4 @@ rem Sauf obligation légale ou contractuelle écrite, le logiciel distribué 
 rem SANS GARANTIES OU CONDITIONS QUELLES QU’ELLES SOIENT, expresses ou implicites.
 rem Consultez la Licence pour les autorisations et les restrictions linguistiques spécifiques relevant de la Licence.
 
-@echo off
 start .\java\bin\javaw -Djavafx.preloader=lu.nowina.nexu.NexUPreLoader -Dglass.accessible.force=false -jar nexu.jar
